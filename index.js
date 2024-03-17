6aad648a57d2541374eba2c73acbba85b0b8e871
Angela DeLeo <rockyangela5@gmail.com>
2024-03-16 21:59:31 -0700

    stored username n password away

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const express = require("express");
 
+const config = require("./config");
+
 const app = express();
 app.use(express.json())
 
-const dbUsername = "atakux";
-const dbPassword = "7JH2iNx316hlqhhA";
-
-const dbURI = `mongodb+srv://${dbUsername}:${dbPassword}@test.vxujlr8.mongodb.net/?retryWrites=true&w=majority&appName=test`;
+const dbURI = `mongodb+srv://${config.dbUsername}:${config.dbPassword}@test.vxujlr8.mongodb.net/?retryWrites=true&w=majority&appName=test`;
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
